5649e219cff2e955ff4217b105a71b2fbfed1014
holy0spirit <107628145+holy0spirit@users.noreply.github.com>
2022-07-14 15:45:19 +0100

    beta version completed

---
+++ main.py
@@ -0,0 +1,29 @@
+from flask import Flask, jsonify
+from bs4 import BeautifulSoup
+import requests
+
+
+app = Flask(__name__)
+
+@app.route('/feed', methods=["GET"])
+def rssFeed():
+    url = 'https://mfmejigboregion7lagos.org/blog/feed/'
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    mItems = soup.find_all('item')
+
+    mFeeds = []
+    for mItem in mItems:
+        mText = mItem.title.get_text()
+        mLink = mItem.guid.get_text()
+        mDescription = mItem.description.get_text()
+
+        mFeed = {
+            "text" : mText,
+            "link" : mLink,
+            "description" : mDescription
+        }
+
+        mFeeds.append(mFeed)
+
+    return jsonify({"feeds" : mFeeds})
